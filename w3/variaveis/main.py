ad7b12229c4d0c0a429a325e7449e2e85a67b61d
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-10 18:07:29 -0300

    estudos w3

---
+++ w3/variaveis/main.py
@@ -0,0 +1,25 @@
+#variavel
+'''
+x = 3
+y = 'natan'
+z = "davi"
+print(x)
+print(y)
+print(z)
+
+#tipo
+print(type(x))
+print(type(y))
+print(type(z))
+
+#atribuição multipla
+x1 = y1 = z1 = 'laranja'
+print(x1, y1, z1)
+'''
+
+#atribuilção em array
+fruits = ["apple", "banana", "cherry"]
+x2, y2, z2 = fruits
+print(x2)
+print(y2)
+print(z2)

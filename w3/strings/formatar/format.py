567734b3548697830bee2dbf265feb3327601ed5
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-11 13:25:43 -0300

    continuação dos estudos

---
+++ w3/strings/formatar/format.py
@@ -0,0 +1,11 @@
+#concatenação
+age = 36
+print ("Minha idade", age)
+
+#cordas
+print(f"Minha idade {age}")
+
+#modificação
+preco = 55.9043
+print(f"O preco é de R$ {preco:.2f}")
+
---
+++ w3/strings/modificar_string/modificar.py
@@ -0,0 +1,18 @@
+#maiuscula
+a = 'hello world'
+print(a.upper())
+
+#minuscula
+print(a.lower())
+
+#remover espaço em branco
+b = '    hello world'
+print(b.strip())
+
+#substitui uma pela outra
+print(a.replace('h','j'))
+
+#retorna uma lista onde o texto inserido sera o separador de cada unidade da lista
+b = a.split(' ')
+print(b)#virou uma lista
+

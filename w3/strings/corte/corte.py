5de145e58c562e1fe1d931c5b7cd07063b452282
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-11 10:51:38 -0300

    estudos em python

---
+++ w3/strings/corte/corte.py
@@ -0,0 +1,11 @@
+#posição inicial inclusa, fina não incluso
+b = "Hello, World!"
+print(b[2:5])#2 incluso, 5 nao incluso
+
+#do início até a posição final, não incluso
+print(b[:5])#inicio até o 5 que nao foi incluido
+
+#fatiar até o fim
+print(b[2:])#inicio incluso, até o final da string
+
+
---
+++ w3/strings/string/string.py
@@ -0,0 +1,15 @@
+
+#len
+a = 'Hello World'
+print(len(a))
+
+#in - 'tem isso' in variavel
+a = 'olá mundo'
+print('olá' in a)
+
+if 'olá' in a:
+    print('Sim, "olá" está na variavel')
+
+print(a[2])
+
+#--------------------------------------------

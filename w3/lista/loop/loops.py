1ebb380bb48c78a3001c365958b118926c177ad0
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-11 23:46:43 -0300

    exercicios python

--- w3/lista/loop/loops.py
+++ w3/lista/loop/loops.py
@@ -1,6 +1,19 @@
+#loop lista
 lista = ["banana", "maça", "pera", "uva"]
 contador = 0
 for x in lista:
-    if "banana" in x:
-        contador+=1
-print(contador)
+    print(x)
+
+#cria nova lista
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+#define um tamanho do loop
+for x in range(10):
+   print(x)
---
+++ w3/lista/ordenar/ordenar.py
@@ -0,0 +1,9 @@
+#Classifique a lista em ordem alfabética:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Classifique a lista numericamente:
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)

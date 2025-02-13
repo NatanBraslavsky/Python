5f25951fc87b98bb53c99c04772914fcdf10e06f
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2025-02-12 21:29:03 -0300

    tde e aula de web

---
+++ faculdade/tdes/tde1/tde.py
@@ -0,0 +1,30 @@
+#1
+num = int(input("Digite um número inteiro: "))
+print(f"O dobro de {num} vale {num*2}")
+
+#2
+largura = float(input("Digite a largura: "))
+altura = float(input("Digite a altura: "))
+print(f"A area vale {largura * altura}:.2f")
+
+#3
+num1 = int(input("Digite um num: "))
+num2 = int(input("Digite outro numero: "))
+print(num1 == num2)
+
+#4
+num14 = int(input("Digite um numero: "))
+num24 = int(input("Digite outro: "))
+print(num14 % num24)
+
+#5 
+num15 = float(input("Digite um numero"))
+num25 = float(input("Digite um numero"))
+num35 = float(input("Digite um numero"))
+media = (num15 + num25 + num35) / 3
+print(media)
+
+#6
+num16 = float(input("Digite um num: "))
+num26 = float(input("Digite outro num: "))
+print(num16 > 10 and num26 < 5)

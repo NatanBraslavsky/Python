a576c08e4e16ba454fdd55c67caf3f05682fe118
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-23 17:38:20 -0300

    estudos python

---
+++ curso_udemy/fundamentos/sets/sets.py
@@ -0,0 +1,27 @@
+
+#?Lógica para tirar eleentos duplicados
+# array = [1,2,3,3,3,3,2,2,2]
+# unico = []
+
+# for elemento in array:
+#     existe = False
+#     for un in unico:
+#         if un == elemento:
+#             existe = True
+#             break
+#     if not existe:
+#         unico.append(elemento)
+
+# print(unico)
+
+#?Sets fazem isso em poucas linhas!
+# lista = [1,2,2,2,3,3,3]
+# print(set(lista))
+
+#?posso converter depois para lista de novo
+# lista = [1,2,2,2,3,3,3]   
+# s1 = set(lista)
+# s2 = list(s1)
+# print(s2)
+
+

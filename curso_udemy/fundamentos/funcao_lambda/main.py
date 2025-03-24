71d985bc1fe85b14486c1253089c659adc3c1dbf
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-24 12:05:28 -0300

    estudos python

--- curso_udemy/fundamentos/funcao_lambda/main.py
+++ curso_udemy/fundamentos/funcao_lambda/main.py
@@ -21,7 +21,20 @@ lista = [
 
 #*Porém tem outra maneiro com a função lambda:
 
-lista.sort(key=lambda item: item['nome'])
-for item in lista:
-    print(item)
+# lista.sort(key=lambda item: item['nome'])
+# for item in lista:
+#     print(item)
+
+#?Lambda serve para criar uma função 'rapida' que nao vamos ficar utilizando toda hora
+
+# def soma(x,y):
+#     return x+y
+# print(soma(2,3))
+# #!ao invés de fazer isso^, faça isso...
+
+soma = lambda x, y : x+y
+print(soma(2,3))
+
+
+
 

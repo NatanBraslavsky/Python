904a6f76e72521f715dec707970a08566e0f424c
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-24 17:47:47 -0300

    estudos

---
+++ curso_udemy/fundamentos/array/list_comprehention.py
@@ -0,0 +1,12 @@
+
+#?maneira comun de adicionar elementos na lista
+
+# lista = []
+# for i in range(10):
+#     lista.append(i)
+
+# print(lista)
+
+#!usando list_comprehention
+lista = [i for i in range(10)]
+print(lista)

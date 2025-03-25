af1503338b9ec8d72b2b6fba36cae96432234861
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-25 16:47:46 -0300

    python estudo

--- curso_udemy/fundamentos/array/list_comprehention.py
+++ curso_udemy/fundamentos/array/list_comprehention.py
@@ -1,5 +1,5 @@
 
-#?maneira comun de adicionar elementos na lista
+#?maneira comum de adicionar elementos na lista
 
 # lista = []
 # for i in range(10):
@@ -8,5 +8,24 @@
 # print(lista)
 
 #!usando list_comprehention
-lista = [i for i in range(10)]
+# lista = [i for i in range(10)]
+# print(lista)
+
+#!Mapeamento list_comprehention
+# produtos = [
+#     {'nome':'p1', 'preco': 20, },
+#     {'nome':'p2', 'preco': 10, },
+#     {'nome':'p3', 'preco': 30, },
+# ]
+
+# novos_produtos = [
+#     {**produto, 'preco':produto['preco']*1.05}
+#     if produto['preco']>20 else {**produto} 
+#     for produto in produtos
+# ]
+
+# print(*novos_produtos, sep='\n')
+
+#!filtro
+lista = [n for n in range(10) if n % 2 == 0]
 print(lista)
---
+++ curso_udemy/fundamentos/array/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+frutas = ['banana', 'kiwi', 'abacaxi']
+novaLista = [x for x in frutas if "a" in x]
+print(novaLista)

d2103737958ca28cd29122ee8db0caaaea719ef7
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-27 19:53:05 -0300

    testes python

---
+++ testes/teste.py
@@ -0,0 +1,14 @@
+
+# soma = lambda x,y : x+y
+# print(soma(5,3))
+
+
+# def soma(x,y):
+#     return x+y
+# print(soma(5,3))
+
+
+num = [1,2,3,4,2,3,3,2,3,]
+num2 = set(num)
+print(num)
+print(num2)

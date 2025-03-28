6cc74a4e2d34557fc96fc2f185e034e06836663f
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-03-27 23:56:15 -0300

    estudos python

--- curso_udemy/fundamentos/try_except/01try.py
+++ curso_udemy/fundamentos/try_except/01try.py
@@ -1,6 +1,22 @@
-digito = input("Digite um número: ")
+# (Parte1) try e except para tratar exceções
+# a = 18
+# b = 0
+# c = a / b
+
 try:
-    digitoFloat = float(digito)#daria erro se eu tentasse converter uma string para um float, por isso o try
-    print("Numero: ", digitoFloat)
-except:
-    print("Isso não é um número.")
+    a = 18
+    b = 0
+    # print(b[0])
+    print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError):
+    print('TypeError + IndexError')
+except Exception:
+    print('ERRO DESCONHECIDO.')
+
+print('CONTINUAR')

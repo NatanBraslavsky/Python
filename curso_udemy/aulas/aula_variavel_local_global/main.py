8cb477e0a21c28841a8c20b54145c65af1f97440
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-04 00:04:54 -0300

    estudos

--- curso_udemy/aulas/aula_variavel_local_global/main.py
+++ curso_udemy/aulas/aula_variavel_local_global/main.py
@@ -10,3 +10,16 @@ c = concatenar('a')
 print(c('b'))
 print(c('c'))
 
+
+
+def soma(x):
+    valorFinal = x
+    def interna(y):
+        nonlocal valorFinal
+        valorFinal = x+y
+        return valorFinal
+    return interna
+
+som = soma(3)
+print(som(4))
+print(som(5))

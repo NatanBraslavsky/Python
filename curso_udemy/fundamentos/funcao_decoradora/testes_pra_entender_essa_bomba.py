8f22edfc14c54aa17b098e23daca3f49d52ddd0d
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-05 20:23:42 -0300

    estudos

--- curso_udemy/fundamentos/funcao_decoradora/testes_pra_entender_essa_bomba.py
+++ curso_udemy/fundamentos/funcao_decoradora/testes_pra_entender_essa_bomba.py
@@ -1,16 +1,35 @@
+# def validarArgumento(condicao):
+#     def decorador(func):
+#         def aninhada(*args, **kwargs):
+#             if condicao(*args, **kwargs):
+#                 return func(*args, **kwargs)
+#             else:
+#                 raise ValueError("Valor inválido.")
+#         return aninhada
+#     return decorador
+
+# @validarArgumento(lambda x: x > 0)
+# def calcularCubo(x):
+#     return x**3
+            
+# cubo = calcularCubo(2)
+# print(cubo)
+
+
 def validarArgumento(condicao):
     def decorador(func):
-        def aninhada(*args, **kwargs):
+        def aninhada(*args,**kwargs):
             if condicao(*args, **kwargs):
-                return func(*args, **kwargs)
+                return func(*args,**kwargs)
             else:
-                raise ValueError("Valor inválido.")
+                raise NameError("Nome inválido.")
         return aninhada
     return decorador
 
-@validarArgumento(lambda x: x > 0)
-def calcularCubo(x):
-    return x**3
-            
-cubo = calcularCubo(2)
-print(cubo)
+@validarArgumento(lambda  x: x == 'natanbraslavsky')
+def printarNome(x):
+    return x
+
+nome = input("Digite o nome: ")
+validar = printarNome(nome)
+print(validar)

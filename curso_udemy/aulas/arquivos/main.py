899e300fa6419752755c9f8b5ebb99ec10480702
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-11 18:01:50 -0300

    estudos

--- curso_udemy/aulas/arquivos/main.py
+++ curso_udemy/aulas/arquivos/main.py
@@ -1,20 +1,27 @@
- #? Criando arquivos com Python + Context Manager with
+import os 
+
+#? Criando arquivos com Python + Context Manager with
  #? Usamos a função open para abrir
  #? um arquivo em Python (ele pode ou não existir)
+
  #? Modos:
  #? r (leitura), w (escrita), x (para criação)
  #? a (escreve ao final), b (binário)
  #? t (modo texto), + (leitura e escrita)
+
  #? Context manager - with (abre e fecha)
+
  #? Métodos úteis
  #? write, read (escrever e ler)
  #? writelines (escrever várias linhas)
  #? seek (move o cursor)
  #? readline (ler linha)
  #? readlines (ler linhas)
+
  #? Vamos falar mais sobre o módulo os, mas:
  #? os.remove ou unlink - apaga o arquivo
  #? os.rename - troca o nome ou move o arquivo
+
  #? Vamos falar mais sobre o módulo json, mas:
  #? json.dump = Gera um arquivo json
  #? json.load
@@ -38,4 +45,4 @@ with open(caminho, 'w+', encoding='utf-8') as arquivo:
     arquivo.seek(0,0)
     print(arquivo.read())
 
-
+#nao deu certo os.rename(caminho, 'aula.txt')

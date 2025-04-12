43291dae14ae7f9d4b0b10faa5170910d7bca32b
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-12 00:06:18 -0300

    estudos

--- testes/json.py
+++ testes/json.py
@@ -1,22 +1,40 @@
-lista = []
+# lista = []
 
+# try:
+#     with open("arquivo.json", "r") as arquivo:
+#         for linha in arquivo:
+#             lista.append(linha)
+# except:
+#     pass
+
+# while True:
+#     num = int(input("Digite um valor(0 para sair): "))
+#     if num == 0:
+#         break
+#     if num in lista:
+#         print("Valor ja na lista.") 
+#     else:
+#         lista.append(num)
+#         with open("arquivo.json", "a") as arquivo:
+#             arquivo.write(f"{num}\n")
+
+
+    
+lista = []
 try:
-    with open("arquivo.json", "r") as arquivo:
+    with open("jsonfile.json", "r") as arquivo:
         for linha in arquivo:
             lista.append(linha)
 except:
     pass
 
 while True:
-    num = int(input("Digite um valor(0 para sair): "))
+    num = int(input("Digite um número(0 para sair.): "))
     if num == 0:
         break
     if num in lista:
-        print("Valor ja na lista.") 
+        print("Número já na lista.")
     else:
         lista.append(num)
-        with open("arquivo.json", "a") as arquivo:
+        with open("jsonfile.json", "a") as arquivo:
             arquivo.write(f"{num}\n")
-
-
-    

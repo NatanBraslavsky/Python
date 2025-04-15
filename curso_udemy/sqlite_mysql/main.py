950be22aa1de353ee26169d1746884160bcfcb9f
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-15 16:29:11 -0300

    estudos sqlite

---
+++ curso_udemy/sqlite_mysql/main.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '(' 
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'neme TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+cursor.close()
+connection.close()

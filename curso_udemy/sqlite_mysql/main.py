54932db3007dd7e6887d212e40148326f24bb903
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-15 17:52:51 -0300

    dbeaver sqlite

--- curso_udemy/sqlite_mysql/main.py
+++ curso_udemy/sqlite_mysql/main.py
@@ -14,11 +14,26 @@ cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '(' 
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    'neme TEXT,'
+    'name TEXT,'
     'weight REAL'
     ')'
 )
 connection.commit()
+
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+#Registrar valores nas colunas da tabela
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) VALUES (NULL, "Luiz Otávio", 9.9), (NULL, "Natan", 5.9)'
+)
+connection.commit()
+
 
 cursor.close()
 connection.close()

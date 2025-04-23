928e549e4cd903ff47cf2a95908242422428434c
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-23 16:47:06 -0300

    estudos django

--- curso_udemy/django/blog/views.py
+++ curso_udemy/django/blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 def exemplo(request):
+
+    context = {
+        'text': 'Olá exemplo.'
+    }
+
     print('exemplo')
-    return render(request, 'blog/exemplo.html')
+    return render(request, 'blog/exemplo.html', context)
 
 def blog(request):
+
+    context = {
+        'text': 'Olá blog'
+    }
+
     print('blog')
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html', context)
 
--- curso_udemy/django/home/views.py
+++ curso_udemy/django/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+context = {
+    'text': 'Texto diferente home.',
+    'title': 'Texto exemplo title'
+}
+
 def home(request):
     print('home')
-    return render(request,'home/index.html')
+    return render(request,'home/index.html', context,)

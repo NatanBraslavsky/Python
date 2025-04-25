598b8da026f414bc835fd749a53b11f1557409a0
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-25 12:19:34 -0300

    estudos django

--- curso_udemy/django/blog/urls.py
+++ curso_udemy/django/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('exemplo/', views.exemplo, name='exemplo')
+    path('post/<int:id>/', views.post, name='post'),
+    path('exemplo/', views.exemplo, name='exemplo'),
 ]
--- curso_udemy/django/blog/views.py
+++ curso_udemy/django/blog/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from blog.data import posts
 
 def exemplo(request):
-
     context = {
         'text': 'Olá exemplo.'
-        
     }
-
     print('exemplo')
     return render(request, 'blog/exemplo.html', context)
 
 def blog(request):
-
     context = {
         'posts': posts
     }
-
     print('blog')
     return render(request, 'blog/index.html', context)
 
+def post(request, id):
+    print('post', id)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)

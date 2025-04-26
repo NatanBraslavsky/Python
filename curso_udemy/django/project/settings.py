1619e8eeeaf2e4cf3f0fa43600ca3014535201b5
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-04-26 13:53:37 -0300

    alterações para teste django estudo

--- curso_udemy/django/project/settings.py
+++ curso_udemy/django/project/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_-(&cs*!g#l$f*so8vm2*57g&xe52gvwhuifu4r!(83sf-xv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

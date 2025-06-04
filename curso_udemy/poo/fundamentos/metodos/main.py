5d3e1821bb7418db68e5384d4755014c9b82c850
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-04 11:10:07 -0300

    escolpo dos metodos

--- curso_udemy/poo/fundamentos/metodos/main.py
+++ curso_udemy/poo/fundamentos/metodos/main.py
@@ -10,3 +10,15 @@ fusca.acelerar()
 
 celta = Carro(nome='celta')
 celta.acelerar()
+
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def comendo(self, alimento):
+        return f'{self.nome} está comendo {alimento}'
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+
+leao = Animal('leão')
+print(leao.comendo('maçã'))

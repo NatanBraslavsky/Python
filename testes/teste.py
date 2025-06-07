8bc392042a99c96609044c577ec41dd98553db14
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-07 14:16:34 -0300

    testes

--- testes/teste.py
+++ testes/teste.py
@@ -94,6 +94,22 @@
 # print(b)
 
 
+# soma = lambda x,y : x+y
+# print(soma(5,3))
+
+
+# def soma(x,y):
+#     return x+y
+# print(soma(5,3))
+
+# def isPar(num):
+#     if num % 2 == 0:
+#         return 'Par'
+#     return 'Ímpar'
+
+# print(isPar(2))
+
+
 soma = lambda x,y : x+y
 print(soma(5,3))
 

5deb2c7226adbefc18e694ca2ae5fd0933da8310
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-14 17:10:25 -0300

    testes set lista

--- testes/teste2.py
+++ testes/teste2.py
@@ -1,14 +1,20 @@
-soma = lambda x,y : x+y
-print(soma(5,3))
+# soma = lambda x,y : x+y
+# print(soma(5,3))
 
 
-def soma(x,y):
-    return x+y
-print(soma(5,3))
+# def soma(x,y):
+#     return x+y
+# print(soma(5,3))
 
-def isPar(num):
-    if num % 2 == 0:
-        return 'Par'
-    return 'Ímpar'
+# def isPar(num):
+#     if num % 2 == 0:
+#         return 'Par'
+#     return 'Ímpar'
 
-print(isPar(2))
+# print(isPar(2))
+
+
+num = [1,2,3,4,2,3,3,2,3,]
+num2 = set(num)
+print(num)
+print(num2)

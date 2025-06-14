ef8663b898cf9cc6de2aabe03e66517c970c56a2
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-13 23:36:29 -0300

    testes para revisao de logica

---
+++ testes/teste2.py
@@ -0,0 +1,14 @@
+soma = lambda x,y : x+y
+print(soma(5,3))
+
+
+def soma(x,y):
+    return x+y
+print(soma(5,3))
+
+def isPar(num):
+    if num % 2 == 0:
+        return 'Par'
+    return 'Ímpar'
+
+print(isPar(2))

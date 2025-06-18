01d8cb339a8c6dba97607858099a6c4a054f8c43
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-17 23:26:25 -0300

    ordenação bruta(nao otimizada)

--- testes/tst.py
+++ testes/tst.py
@@ -9,3 +9,23 @@ def quicksort(arr):
         maior = [i for i in arr[1:] if i > pivo]
         return quicksort(menor) + [pivo] + quicksort(maior)
 print(quicksort([5,4,3,3,5,3,1]))
+
+
+
+def acharMenor(lista):
+    menor = lista[0]
+    indiceMenor = 0
+    for i in range(1, len(lista)):
+        if lista[i] < menor:
+            menor = lista[i]
+            indiceMenor = i
+    return indiceMenor
+
+def ordenarLista(lista):
+    novaLista = []
+    for i in range(len(lista)):
+        menor = acharMenor(lista)
+        novaLista.append(lista.pop(menor))
+    return novaLista
+
+print(ordenarLista([3,5,1,2]))

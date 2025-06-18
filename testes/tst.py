0486836a980608aa3029f8d4bdae890e63e54397
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-17 23:18:08 -0300

    faculdade ex tde

--- testes/tst.py
+++ testes/tst.py
@@ -6,3 +6,10 @@ def singleNumber(lista):
 
 lista = [1,1,2,2,4,5]
 print(singleNumber(lista))
+
+dados = (3, 10.5, 'natan', True)
+print(f"1: {dados[1]}, 2: {dados[3]}")
+try:
+    dados[0] = dados[2]
+except:
+    print("Não é possivel modificar os valores de uma tupla.")

bc730bfb0ae5b9c0ce58c4799a9ca61886fa1ed1
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-21 22:38:43 -0300

    leetcode

---
+++ testes/verificarLeet.py
@@ -0,0 +1,11 @@
+def goodpar(nums):
+    qtdGoodPar = 0
+    tamanhoArray = len(nums)
+    for i in range(tamanhoArray):
+        for j in range(i+1,tamanhoArray):
+            if nums[i] == nums[j]:
+                qtdGoodPar+=1
+    return qtdGoodPar
+
+arr = [1,2,3]
+print(goodpar(arr))

a039896290576dfed6f4e0394bc251a99ad4ec10
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-30 22:46:38 -0300

    recursividade teste

--- testes/tst.py
+++ testes/tst.py
@@ -29,3 +29,13 @@ def ordenarLista(lista):
     return novaLista
 
 print(ordenarLista([3,5,1,2]))
+
+
+
+def contar_elementos(arr):
+    if arr == []:
+        return 0
+    else:
+        return 1 + contar_elementos(arr[1:])
+    
+print(contar_elementos([1,5,3]))

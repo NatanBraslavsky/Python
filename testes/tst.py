51697fd7227ee1ac4383165ad905181a791cab84
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-01 22:47:20 -0300

    teste date

--- testes/tst.py
+++ testes/tst.py
@@ -39,3 +39,18 @@ def contar_elementos(arr):
         return 1 + contar_elementos(arr[1:])
     
 print(contar_elementos([1,5,3]))
+
+
+from datetime import date
+atual = date.today().year
+totalmaior = 0
+totalmenor = 0
+for pessoas in range(1 , 8):
+    nasc = int(input('Em que ano a {}º pessoa nasceu? '.format(pessoas)))
+    idade = atual - nasc
+    if idade >= 21:
+        totalmaior += 1
+    else:
+        totalmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totalmaior))
+print('E também tivemos {} pessoas menores de idade'.format(totalmenor))
